eaa2842688e88ef7a97bace3fec1f40c92767fb6
jmeadows17 <J.C.Meadows@hotmail.com>
2023-07-15 16:04:35 +0100

    Update derivation_metric.py

--- derivation_metric.py
+++ derivation_metric.py
@@ -5,63 +5,51 @@ import numpy as np
 from itertools import combinations
 
 def balanced_brackets(eq):
-    bracket_map = {"(": ")", "[": "]", "{": "}"}
-    open_brackets = set(["(", "[", "{"])
-    stack = []
-
+    a, b, c = 0, 0, 0
     for i in eq:
-        if i in open_brackets:
-            stack.append(i)
-        elif stack and i == bracket_map[stack[-1]]:
-            stack.pop()
-        else:
-            return False
-    return not stack
+        if i == "(":
+            a += 1
+        if i == "{":
+            b += 1
+        if i == "[":
+            c += 1
+        if i == ")":
+            a -= 1
+        if i == "}":
+            b -= 1
+        if i == "]":
+            c -= 1
+    if a != 0 or b != 0 or c != 0:
+        return False
+    else:
+        return True
 
-def fix_brackets(eq):
-    bracket_map = {"(": ")", "[": "]", "{": "}"}
-    open_brackets = set(["(", "[", "{"])
-    stack = []
-    fixed_eq = ""
-
-    for i in eq:
-        if i in open_brackets:
-            stack.append(i)
-        elif stack and i == bracket_map[stack[-1]]:
-            stack.pop()
-        elif not stack and i in bracket_map.values():
-            continue
-
-        fixed_eq += i
-
-        while stack and bracket_map[stack[-1]] not in fixed_eq:
-            fixed_eq += bracket_map[stack.pop()]
-
-    while stack:
-        fixed_eq += bracket_map[stack.pop()]
-
-    return fixed_eq
-
-def score(ref, pred, metric_name, metric, fix=False):
-    if fix:
-        pred = fix_brackets(pred)
-    elif not balanced_brackets(pred) or '=' not in pred:
+def score(ref, pred, metric_name, metric):
+    if not balanced_brackets(pred) or '=' not in pred:
         return 0
 
-    # Check if LHS and RHS are identical
-    lhs, rhs = pred.split('=')
-    if lhs.strip() == rhs.strip():
+    #Check if LHS and RHS are identical
+
+    try:
+        lhs, rhs = pred.split('=')
+        if lhs.replace(" ","") == rhs.replace(" ",""):
+            return 0
+    except:
+        pass
+        
+    
+    try:
+        if metric_name == "rouge":
+            score = metric.compute(predictions=[pred],references=[ref])['rouge2']
+        if metric_name == "bleu":
+            score = metric.compute(predictions=[pred],references=[ref])['bleu']
+        if metric_name == "gleu":
+            score = metric.compute(predictions=[pred],references=[ref])['google_bleu']
+        return score
+    except:
         return 0
 
-    if metric_name == "rouge":
-        score = metric.compute(predictions=[pred],references=[ref])['rouge2']
-    if metric_name == "bleu":
-        score = metric.compute(predictions=[pred],references=[ref])['bleu']
-    if metric_name == "gleu":
-        score = metric.compute(predictions=[pred],references=[ref])['google_bleu']
-    return score
-
-def optimize_score(ref, pred, metric_name, fix=False, return_equations=False):
+def optimize_score(ref, pred, metric_name, return_equations=False):
     
     if metric_name == "gleu":
         metric = load("google_bleu")
@@ -93,7 +81,7 @@ def optimize_score(ref, pred, metric_name, fix=False, return_equations=False):
             for pos in positions:
                 pred_copy.insert(pos, '')
         
-        scores = [score(ref, pred, metric_name, metric, fix=fix) for ref, pred in zip(ref_copy, pred_copy)]
+        scores = [score(ref, pred, metric_name, metric) for ref, pred in zip(ref_copy, pred_copy)]
         avg_scores.append(np.mean(scores))
         alignments.append((ref_copy, pred_copy))
     
